4473416454499b0707f4849a63979e2627f5298f
TobiasHasenkamp <71656268+TobiasHasenkamp@users.noreply.github.com>
2020-10-02 15:49:30 +0200

    Update OrderService.java

--- src/main/java/de/neuefische/ordersystem/OrderService.java
+++ src/main/java/de/neuefische/ordersystem/OrderService.java
@@ -11,11 +11,7 @@ public class OrderService {
     public void generateNewOrder(ArrayList<Product> products, OrderDB orderDb) {
         Order newOrder = new Order();
         for (Product product: products) {
-            if (product.getName().isEmpty()){
-                System.out.println("Failure");
-            } else{
-                newOrder.addProducts(product);
-            }
+               newOrder.addProducts(product);
         }
         orderDb.addOrder(newOrder);
     }
